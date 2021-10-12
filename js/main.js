6b6492e612d7ff4a66e6432a57c4ed1b2de857d6
Margarita Kachalko <margarita.kachalko@gmail.com>
2021-10-12 15:20:43 +0300

    Created: modal window

--- js/main.js
+++ js/main.js
@@ -19,4 +19,70 @@ $(document).ready(function () {
      
    });
 
+  var modalButton = $('[data-toggle=modal]');
+  var closeButton = $('[data-toggle=close]');
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  var body = $('body');
+
+  modalButton.on('click', openModal);
+  closeButton.on('click', closeModal);
+
+  function openModal() {
+    modalOverlay.addClass('modal__overlay_visible');
+    modalDialog.addClass('modal__dialog_visible');
+    body.addClass('lock');
+  }
+  function closeModal(event) {
+    
+    event.preventDefault()
+    modalOverlay.removeClass('modal__overlay_visible');
+    modalDialog.removeClass('modal__dialog_visible');
+    body.removeClass('lock');
+    
+  }
+
+  $(document).on('keydown', function(event){
+    if(event.key == 'Escape'){
+      event.preventDefault();
+      modalOverlay.removeClass('modal__overlay_visible');
+      modalDialog.removeClass('modal__dialog_visible');
+      body.removeClass('lock');
+    }
+  });
+
+  $('.form').each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        name: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true,
+          minlength: 18
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите своё имя"
+        },
+        phone: {
+          required: "Пожалуйста, укажите номер телефона",
+          minlength:"Ваш номер должен состоять из 11 цифр"
+        },
+        email: {
+          required: "Пожалуйста, укажите свой email",
+          email: "Ваш email должен вводиться в формате name@domain.com"
+        }
+      }
+    });
+  });
+
+  $('[name=phone]').mask('+999 (99) 999-99-99', {
+    translation: {9: {pattern: /[0-9]/}}
+  });
+
 });
